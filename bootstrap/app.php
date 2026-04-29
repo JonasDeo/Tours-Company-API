73f18e8acd504f2260ea0b57fd6d47c1892710b6
JonasDeo <jonasdeo02@gmail.com>
2026-04-29 16:42:52 +0300

    kernel and cors fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
         $middleware->alias([
             'auth' => \App\Http\Middleware\Authenticate::class,
             'jwt.auth' => \App\Http\Middleware\JwtMiddleware::class,
